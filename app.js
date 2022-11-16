42d9b3072337b69519aedbdaed0dac1d24eba589
kodyyuen <kody.yuen@gmail.com>
2022-11-16 18:17:25 -0500

    added back options for mongoose connecting

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    socketTimeoutMS: 45000,
+    family: 4
+}
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, options);
 const app = express()
 app.use(cors());
 app.use(express.json());
